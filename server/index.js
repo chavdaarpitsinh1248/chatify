31b62a7562062d224eacb8cb577ce00ca372ed8d
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-12-15 18:34:50 +0530

    feat: add socket.io real-time chat server

---
+++ server/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+    },
+});
+
+let users = {};
+
+io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    socket.on("join", (username) => {
+        users[socket.id] = username;
+        io.emit("users", Object.values(users));
+    });
+
+    socket.on("sendMessage", (message) => {
+        io.emit("receiveMessage", {
+            ...message,
+            id: Date.now(),
+            timestamp: new Date(),
+        });
+    });
+
+    socket.on("disconnect", () => {
+        delete users[socket.id];
+        io.emit("users", Object.values(users));
+        console.log("User disconnected");
+    });
+});
+
+server.listen(5000, () =>
+    console.log("Server running on http://localhost:5000")
+);
