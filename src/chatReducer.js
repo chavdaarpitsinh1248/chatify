b474c8ac4f5f86a08c40ad11182d32ddd37e6709
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-12-15 16:10:45 +0530

    chatReducer function

---
+++ src/chatReducer.js
@@ -0,0 +1,22 @@
+export const initialState = {
+    messages: [
+        {
+            id: 1,
+            text: "Hi! 👋 Welcome to Chatify",
+            sender: "bot",
+            timestamp: new Date(),
+        },
+    ],
+};
+
+export function chatReducer(state, action) {
+    switch (action.type) {
+        case "SEND_MESSAGE":
+            return {
+                ...state,
+                messages: [...state.messages, action.payload],
+            };
+
+        default: return state;
+    }
+}
