6897e8a88220008ca252218138098323b5040f17
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-12-15 16:13:15 +0530

    updated to add chatreducer

--- src/ChatApp.jsx
+++ src/ChatApp.jsx
@@ -1,13 +1,18 @@
+import { useReducer } from "react";
 import ChatHeader from "./components/ChatHeader";
 import ChatMessages from "./components/ChatMessages";
 import ChatInput from "./components/ChatInput";
+import { chatReducer, initialState } from "./chatReducer";
+
 
 export default function ChatApp() {
+    const [state, dispatch] = useReducer(chatReducer, initialState);
+
     return (
         <div className="h-screen flex flex-col max-w-md mx-auto border-x bg-white">
             <ChatHeader />
-            <ChatMessages />
-            <ChatInput />
+            <ChatMessages messages={state.messages} />
+            <ChatInput dispatch={dispatch} />
         </div>
     );
 }
