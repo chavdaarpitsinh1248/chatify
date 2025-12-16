fbeb70987985100ec67c157c6bbfbad898908474
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-12-16 15:45:12 +0530

    feat: protect socket.io connention with JWT

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 app.use(cors());
@@ -29,8 +30,26 @@ mongoose
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.error(err));
 
+
+io.use((socket, next) => {
+    const token = socket.handshake.auth?.token;
+
+    if (!token) {
+        return next(new Error("Authentication error"));
+    }
+
+    try {
+        const decoded = JsonWebTokenError.verify(token, process.env.JWT_SECRET);
+        socket.user = decoded; // { id, email }
+        next();
+    } catch (err) {
+        next(new Error("Authentication error"));
+    }
+});
+
+
 io.on("connection", (socket) => {
-    console.log("User connected:", socket.id);
+    console.log("User connected:", socket.user.email);
 
     socket.on("join", (username) => {
         users[socket.id] = username;
