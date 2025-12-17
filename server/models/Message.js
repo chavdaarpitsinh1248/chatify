9a8c3b9f3d8aaf03839b0326c3dae692bba32f55
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-12-17 19:38:04 +0530

    feat: create Message schema for persistent channel messages

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,34 +1,11 @@
 const mongoose = require("mongoose");
 
-const MessageSchema = new mongoose.Schema(
-    {
-        serverId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Server",
-            required: true,
-        },
-        channelId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Channel",
-            required: true,
-        },
-        sender: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
-        senderName: {
-            type: String,
-            required: true,
-        },
-        text: {
-            type: String,
-            trim: true,
-            required: true,
-            maxlength: 2000,
-        },
-    },
-    { timestamps: true }
-);
+const MessageSchema = new mongoose.Schema({
+    serverId: { type: mongoose.Schema.Types.ObjectId, ref: "Server", required: true },
+    channelId: { type: mongoose.Schema.Types.ObjectId, required: true },
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    text: { type: String, required: true },
+    timestamp: { type: Date, default: Date.now },
+});
 
-module.exports = mongoose.model("Message", MessageSchema.index({ serverId: 1, channel: 1, createdAt: 1 }));
+module.exports = mongoose.model("Message", MessageSchema);
