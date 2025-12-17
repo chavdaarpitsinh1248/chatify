636dae7169b5dd35dd4496c20c528edccd9e71d7
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-12-17 18:21:35 +0530

    feat: matching API_URL structure, small improvement

--- client/src/api/serverApi.js
+++ client/src/api/serverApi.js
@@ -1,9 +1,27 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/servers";
+const API = `${import.meta.env.VITE_API_URL || "http://localhost:5000"}/api/servers`;
 
-export const createServer = (name, token) =>
-    axios.post(API, { name }, { headers: { Authorization: `Bearer ${token}` } })
+export const createServer = async (name, token) => {
+    try {
+        const res = await axios.post(
+            API,
+            { name },
+            { headers: { Authorization: `Bearer ${token}` } }
+        );
+        return res.data;
+    } catch (err) {
+        throw err.response?.data || err;
+    }
+};
 
-export const getServers = (token) =>
-    axios.get(API, { headers: { Authorization: `Bearer ${token}` } });
+export const getServers = async (token) => {
+    try {
+        const res = await axios.get(API, {
+            headers: { Authorization: `Bearer ${token}` },
+        });
+        return res.data;
+    } catch (err) {
+        throw err.response?.data || err;
+    }
+};
