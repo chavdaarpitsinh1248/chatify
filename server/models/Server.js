02dc3fc1b79defef2dc735919167715bf05105fd
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-12-17 16:20:07 +0530

    feat: no bugs. no refactor. small improvements

--- server/models/Server.js
+++ server/models/Server.js
@@ -1,15 +1,29 @@
 const mongoose = require("mongoose");
 
-const ChannelSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    type: { type: String, enum: ["text", "voice"], default: "text" },
-});
+const ChannelSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        type: { type: String, enum: ["text", "voice"], default: "text" },
+    },
+    { _id: true }
+);
 
-const ServerSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    channels: [ChannelSchema],
-    members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+const ServerSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true, trim: true },
+        owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+        channels: [ChannelSchema],
+        members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    },
+    { timestamps: true }
+);
+
+ServerSchema.pre("save", function (next) {
+    if (!this.members) this.members = [];
+    if (!this.members.includes(this.owner)) {
+        this.members.push(this.owner);
+    }
+    next();
 });
 
 module.exports = mongoose.model("Server", ServerSchema);
