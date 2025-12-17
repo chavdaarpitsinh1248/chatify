cdc8f920b227f8d650477a79ba394e9df0ede723
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-12-17 16:25:57 +0530

    feat: no bugs, no refactor, small improvements

--- server/models/User.js
+++ server/models/User.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
+        trim: true,
         minlength: 5,
         maxlength: 20,
     },
@@ -14,14 +15,22 @@ const UserSchema = new mongoose.Schema({
         unique: true,
         lowercase: true,
         trim: true,
+        match: /.+\@.+\..+/,
     },
     password: {
         type: String,
         required: true,
+        select: false,
     },
+    avatar: {
+        type: String,
+    }
 
 },
     { timestamps: true }
 );
+
+UserSchema.index({ username: 1 }, { unique: true });
+UserSchema.index({ email: 1 }, { unique: true });
 
 module.exports = mongoose.model("User", UserSchema);
