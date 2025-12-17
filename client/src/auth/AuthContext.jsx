c0b9cf22c7b2794bc1b078293213325e3c3f558e
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-12-17 18:24:58 +0530

    feat: improve nameing sense, avoiding Naming Collision

--- client/src/auth/AuthContext.jsx
+++ client/src/auth/AuthContext.jsx
@@ -3,27 +3,38 @@ import { createContext, useContext, useState, useEffect } from "react";
 const AuthContext = createContext(null);
 
 export function AuthProvider({ children }) {
-    const [user, setUser] = useState(null);
+    const [auth, setAuth] = useState(null);
 
     useEffect(() => {
-        const stored = localStorage.getItem("user");
-        if (stored) setUser(JSON.parse(stored));
+        try {
+            const stored = localStorage.getItem("user");
+            if (stored) setAuth(JSON.parse(stored));
+        } catch {
+            localStorage.removeItem("user");
+        }
     }, []);
 
     const login = ({ token, user }) => {
         const authData = { token, user };
-        setUser(authData);
+        setAuth(authData);
         localStorage.setItem("user", JSON.stringify(authData));
     };
 
-
     const logout = () => {
-        setUser(null);
+        setAuth(null);
         localStorage.removeItem("user");
     };
 
     return (
-        <AuthContext.Provider value={{ user, login, logout }}>
+        <AuthContext.Provider
+            value={{
+                auth,
+                token: auth?.token,
+                user: auth?.user,
+                login,
+                logout,
+            }}
+        >
             {children}
         </AuthContext.Provider>
     );
@@ -34,4 +45,3 @@ export const useAuth = () => {
     if (!ctx) throw new Error("useAuth must be used inside AuthProvider");
     return ctx;
 };
-
