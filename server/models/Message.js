d6301ac441855a66fcae0c94bdb6a356e929b037
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-12-17 19:45:32 +0530

    back to improved version

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,11 +1,34 @@
 const mongoose = require("mongoose");
 
-const MessageSchema = new mongoose.Schema({
-    serverId: { type: mongoose.Schema.Types.ObjectId, ref: "Server", required: true },
-    channelId: { type: mongoose.Schema.Types.ObjectId, required: true },
-    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    text: { type: String, required: true },
-    timestamp: { type: Date, default: Date.now },
-});
+const MessageSchema = new mongoose.Schema(
+    {
+        serverId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Server",
+            required: true,
+        },
+        channelId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Channel",
+            required: true,
+        },
+        sender: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        senderName: {
+            type: String,
+            required: true,
+        },
+        text: {
+            type: String,
+            trim: true,
+            required: true,
+            maxlength: 2000,
+        },
+    },
+    { timestamps: true }
+);
 
-module.exports = mongoose.model("Message", MessageSchema);
+module.exports = mongoose.model("Message", MessageSchema.index({ serverId: 1, channel: 1, createdAt: 1 }));
