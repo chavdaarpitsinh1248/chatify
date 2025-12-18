ba4b992230b35e798d313fd8951d64d6fe9db94d
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-12-18 15:50:57 +0530

    feat: join and leave socket rooms on channel change

--- client/src/components/ChatPanel.jsx
+++ client/src/components/ChatPanel.jsx
@@ -6,31 +6,55 @@ import ChatInput from "./ChatInput";
 export default function ChatPanel({ server, channel, socket, token, user }) {
     const [messages, setMessages] = useState([]);
 
+    // Join / leave channel socket room
     useEffect(() => {
-        if (!server || !channel) return;
-
-        setMessages([]); // reset when switching channels
-
-        getChannelMessages(server._id, channel._id, token)
-            .then((res) => setMessages(res.data))
-            .catch(() => console.error("Failed to fetch messages"));
+        if (!channel) return;
 
         socket.emit("joinChannel", {
-            serverId: server._id,
             channelId: channel._id,
         });
 
-        const handleNewMessage = (msg) => setMessages((prev) => [...prev, msg]);
+        return () => {
+            socket.emit("leaveChannel", {
+                channelId: channel._id,
+            });
+        };
+    }, [channel, socket]);
+
+    // Load message history + realtime updates
+    useEffect(() => {
+        if (!channel) return;
+
+        setMessages([]); // reset when switching channel
+
+        getChannelMessages(server._id, channel._id, token)
+            .then(res => setMessages(res.data))
+            .catch(err => console.error(err));
+
+        const handleNewMessage = (msg) => {
+            if (msg.channelId === channel._id) {
+                setMessages(prev => [...prev, msg]);
+            }
+        };
+
         socket.on("newChannelMessage", handleNewMessage);
 
-        return () => socket.off("newChannelMessage", handleNewMessage);
-    }, [server?._id, channel?._id, socket, token]);
+        return () => {
+            socket.off("newChannelMessage", handleNewMessage);
+        };
+    }, [channel, server, token, socket]);
 
-    if (!server || !channel) return <div className="p-4">Select a channel</div>;
+    if (!server || !channel) {
+        return <div className="p-4">Select a channel</div>;
+    }
 
     return (
         <div className="flex-1 flex flex-col h-full">
-            <ChatMessages messages={messages} currentUserId={user.id} />
+            <ChatMessages
+                messages={messages}
+                currentUserId={user.id}
+            />
+
             <ChatInput
                 socket={socket}
                 user={user}
